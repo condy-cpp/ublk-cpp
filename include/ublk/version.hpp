a3552032ac2fc0bc509d41370f5f88e67361f766
wokron <yi1tang.yang@gmail.com>
2026-08-29 20:01:45 +0800

    add version.hpp

---
+++ include/ublk/version.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file version.hpp
+ * @brief Library version macros and constants.
+ */
+
+#pragma once
+
+// TODO
+/** @brief ublk-cpp major version. */
+#define UBLKCPP_VERSION_MAJOR 0
+
+/** @brief ublk-cpp minor version. */
+#define UBLKCPP_VERSION_MINOR 0
+
+namespace ublk {
+
+/** @brief ublk-cpp major version. */
+inline constexpr int version_major = UBLKCPP_VERSION_MAJOR;
+
+/** @brief ublk-cpp minor version. */
+inline constexpr int version_minor = UBLKCPP_VERSION_MINOR;
+
+} // namespace ublk
