92670ea1acdea3435858df5710077f2edb1a2987
wokron <yi1tang.yang@gmail.com>
2026-08-29 19:59:43 +0800

    update ublk.hpp

--- include/ublk.hpp
+++ include/ublk.hpp
@@ -1,1 +1,21 @@
+/**
+ * @file ublk.hpp
+ * @brief Main include file for the ublk-cpp library.
+ */
+
 #pragma once
+
+#include "ublk/control.hpp" // IWYU pragma: export
+#include "ublk/daemon.hpp"  // IWYU pragma: export
+#include "ublk/handler.hpp" // IWYU pragma: export
+#include "ublk/helpers.hpp" // IWYU pragma: export
+#include "ublk/io.hpp"      // IWYU pragma: export
+#include "ublk/query.hpp"   // IWYU pragma: export
+#include "ublk/raw.hpp"     // IWYU pragma: export
+#include "ublk/runtime.hpp" // IWYU pragma: export
+#include "ublk/version.hpp" // IWYU pragma: export
+
+/**
+ * @brief The main namespace of the ublk-cpp library.
+ */
+namespace ublk {}
